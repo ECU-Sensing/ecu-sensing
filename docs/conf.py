49a1f22d55199ddf9dd7d21bbfc239d8576178d1
ColbySawyer7 <sawyerc17@students.ecu.edu>
2022-02-01 15:09:09 -0500

    Added theme

--- docs/conf.py
+++ docs/conf.py
@@ -69,3 +69,20 @@ todo_include_todos = False
 
 # If this is True, todo emits a warning for each TODO entries. The default is False.
 todo_emit_warnings = True
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    try:
+        import sphinx_rtd_theme
+
+        html_theme = "sphinx_rtd_theme"
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), "."]
+    except:
+        html_theme = "default"
+        html_theme_path = ["."]
+else:
+    html_theme_path = ["."]
